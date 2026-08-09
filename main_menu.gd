0804267edfc7ea8a4610bfc8634b4316c0f0a76d
debelique <n.m.marov@gmail.com>
2026-08-09 18:52:25 +0100

    Main Menu and Escape
    Added main menu functionality, clicking play redirects to the scene and clicking escape in the game bring up back the main menu.

--- main_menu.gd
+++ main_menu.gd
@@ -1,16 +1,20 @@
 extends Control
 
+# Play redirects to the scene
+const GAME_SCENE_PATH = "res://node_2d.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$VBoxContainer/ButtonPlay.pressed.connect(_on_button_play_pressed)
+	$VBoxContainer/ButtonQuit.pressed.connect(_on_button_quit_pressed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-	
+
+func _on_button_play_pressed() -> void:
+	get_tree().change_scene_to_file(GAME_SCENE_PATH)
+
 func _on_button_quit_pressed() -> void:
 	get_tree().quit()
 	pass # Replace with function body.
