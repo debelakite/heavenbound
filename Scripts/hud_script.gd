c725dd6c409914efbf58a2b9523286f5833eb474
nedev-dev <martinnedev711@gmail.com>
2026-08-21 10:09:11 +0300

    main HUD added,

---
+++ Scripts/hud_script.gd
@@ -0,0 +1,20 @@
+extends CanvasLayer
+
+var player: Player = null
+
+func _ready() -> void:
+	call_deferred("find_player")
+
+func find_player() -> void:
+	player = get_tree().get_first_node_in_group("player")
+	if player:
+		player.health_changed.connect(_on_health_changed)
+
+func _on_health_changed(new_stage: int) -> void:
+	# update health UI
+	pass
+
+func _process(_delta: float) -> void:
+	if player and player.dash_cooldown > 0.0:
+		# update dash cooldown indicator using player.dash_cooldown_timer / player.dash_cooldown
+		pass
